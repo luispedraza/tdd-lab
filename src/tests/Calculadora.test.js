8f773f1a1cc7f9a0e9afa5a952a328dc0946ae27
Luis Pedraza <luispedraza@gmail.com>
2022-05-19 18:58:03 +0200

    iniciada la prueba de la raiz

--- src/tests/Calculadora.test.js
+++ src/tests/Calculadora.test.js
@@ -16,3 +16,14 @@ test.each(cases)('probar que %p + %p vale %p', async (a, b, r) => {
     // asser
     expect(resultado).toBe(r);
  })
+
+ test('la raiz de 33 es 5.744562647 con una precision de 0.001', async () => { 
+     const calc = new Calculadora();
+     const valor = 33;
+     const resultadoEsperado = 5.744562647;
+     // act
+     const resultado = calc.raiz(valor);
+     // assert
+     expect(resultado).tobeCloseTo(resultadoEsperado, 3);
+
+  })
